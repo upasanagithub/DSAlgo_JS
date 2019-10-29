2d37774c00c9dcc6f3c84098efe5767e6cb77224
upasana.mahanta <ups.win@gmail.com>
2019-10-29 21:36:21 +0530

    commit

--- preorderTreeTraversal.js
+++ preorderTreeTraversal.js
@@ -13,19 +13,31 @@ class Node{
 
 }
 
-function preorderTraverse(node){
-  if(node == null)
-    return;
-  console.log(node.element);
-  preorderTraverse(node.left);
-  preorderTraverse(node.right);
+class bt{
+  constructor(){
+    this.root = null;
+  }
+  buildSampleTree(){
+    console.log("here");
+    if(this.root == null)
+      this.root = new Node(3);
+    var current = this.root;
+    current.left = new Node(4);
+    current.right = new Node(6);
+    current.left.left = new Node(5);
+    current.left.right = new Node(7);
+
+  }
+  preorderTraverse(node){
+
+    if(node == null)
+      return;
+    console.log(node.element);
+    this.preorderTraverse(node.left);
+    this.preorderTraverse(node.right);
+  }
 }
 
-var tr = new Node(3);
-var tr1 = tr;
-tr.left = new Node(4);
-tr.right = new Node(6);
-tr.left.left = new Node(5);
-tr.left.right = new Node(7);
-
-preorderTraverse(tr1);
+var bTree = new bt();
+bTree.buildSampleTree();
+bTree.preorderTraverse(bTree.root);
