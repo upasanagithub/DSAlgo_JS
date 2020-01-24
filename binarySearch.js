e6899467c5b4cd30530cf8f52c8539152964c55e
upasana.mahanta <upasana.mahanta@servicenow.com>
2020-01-24 17:31:48 +0530

    BSearch

--- binarySearch.js
+++ binarySearch.js
@@ -4,22 +4,26 @@ var ele = 20;
 var start = 0;
 var end = arr.length-1;
 var found = 0;
+if(ele > arr[arr.length-1])
+  console.log("Element not found");
 
-while(end>=start && found == 0){
+else{
+  while(end>=start && found == 0){
 
-  var med = parseInt((start+(end-start)/2),10);
+    var med = parseInt((start+(end-start)/2),10);
 
-  console.log(start+"  "+med+"  "+end);
+    console.log(start+"  "+med+"  "+end);
 
-  if(ele == arr[med]){
-    console.log("Element found at postion: "+(med+1));
-    found = 1;
+    if(ele == arr[med]){
+      console.log("Element found at postion: "+(med+1));
+      found = 1;
+    }
+    else if(ele < arr[med]){
+      end = med-1;
+    }
+    else{
+      start = med+1;
+    }
+
   }
-  else if(ele < arr[med]){
-    end = med-1;
-  }
-  else{
-    start = med+1;
-  }
-
 }
